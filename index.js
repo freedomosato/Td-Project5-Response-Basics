c9cf340f16e27bb19580b6c749f2a92958db9dff
Freedom Osato <osatohanmwen.f.i@gmail.com>
2023-10-30 13:50:26 +0100

    My Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let navEl = document.getElementById("nav");
+
+console.log(navEl);
+
+function faBars() {
+  if (navEl.style.display === "block") {
+    navEl.style.display = "none";
+  } else{
+    navEl.style.display = "block";
+  }
+    console.log(4-2)
+}
